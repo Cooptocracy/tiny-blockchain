ee8072501ba5df3fe62c186d6a492637f54ef920
srihari kapu <sriharikapu163@gmail.com>
2020-07-30 14:28:37 +0530

    Create Generator.java

---
+++ JAVA/Generator.java
@@ -0,0 +1,22 @@
+package tiny.blockchain;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Generator {
+    public static String generateHash(String text) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+            String encoded = Base64.getEncoder().encodeToString(hash);
+            return encoded;
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Generator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
